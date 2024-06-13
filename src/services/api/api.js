538f0693c39fa608a2b9c77c59f355c7ad207d8b
thuanCEO <dohuuthuan.bhdn@gmail.com>
2024-06-13 12:59:20 +0700

    test

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  // baseURL: 'https://3.27.235.74/',
-  baseURL: "https://localhost:7170/",
+  baseURL: "http://realityprint.somee.com/",
+
+  // baseURL: "https://localhost:7170/",
   headers: {
     "Content-Type": "application/json",
     "content-type": "text/plain",
